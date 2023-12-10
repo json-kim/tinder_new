4a125e030d7ae4c14fa17b5e8e6c48036ab8d85f
flutter_dart@kakao.com <flutter_dart@kakao.com>
2023-12-10 23:45:37 +0900

    Update 이미지 컬러 변경

--- lib/util/constants.dart
+++ lib/util/constants.dart
@@ -5,14 +5,14 @@ const kDefaultPadding = EdgeInsets.symmetric(
   horizontal: 26.0,
 );
 
-const kPrimaryColor = Color(0xFF1D1C1C);
+const kPrimaryColor = Color(0xFFEB6271);
 const kSecondaryColor = Colors.white;
 const kAccentColor = Color(0xFFF4C470);
 
 const kColorPrimaryVariant = Color(0xFF424242);
 
-const kBackgroundColor = Color(0xFF1D1C1C);
-const kBackgroundColorInt = 0xFF1D1C1C;
+const kBackgroundColor = Color(0xFFEB6271);
+const kBackgroundColorInt = 0xFFEB6271;
 const kFontFamily = 'Nunito';
 
 const kTextTheme = TextTheme(
@@ -31,14 +31,14 @@ const kButtonTheme = ButtonThemeData(
 );
 
 const Map<int, Color> kThemeMaterialColor = {
-  50: Color.fromRGBO(0, 0, 0, .1),
-  100: Color.fromRGBO(0, 0, 0, .2),
-  200: Color.fromRGBO(0, 0, 0, .3),
-  300: Color.fromRGBO(0, 0, 0, .4),
-  000: Color.fromRGBO(0, 0, 0, .5),
-  500: Color.fromRGBO(0, 0, 0, .6),
-  600: Color.fromRGBO(0, 0, 0, .7),
-  700: Color.fromRGBO(0, 0, 0, .8),
-  800: Color.fromRGBO(0, 0, 0, .9),
-  900: Color.fromRGBO(0, 0, 0, 1),
+  50: Color.fromRGBO(235, 98, 113, .1),
+  100: Color.fromRGBO(235, 98, 113, .2),
+  200: Color.fromRGBO(235, 98, 113, .3),
+  300: Color.fromRGBO(235, 98, 113, .4),
+  000: Color.fromRGBO(235, 98, 113, .5),
+  500: Color.fromRGBO(235, 98, 113, .6),
+  600: Color.fromRGBO(235, 98, 113, .7),
+  700: Color.fromRGBO(235, 98, 113, .8),
+  800: Color.fromRGBO(235, 98, 113, .9),
+  900: Color.fromRGBO(235, 98, 113, 1),
 };
