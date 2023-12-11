d9729422699e25a663fd900f06a1f4ed1d9f0c7a
flutter_dart@kakao.com <flutter_dart@kakao.com>
2023-12-12 05:13:26 +0900

    Add FirebaseAuthSource.signInWithGoogle 메서드 추가

--- lib/data/db/remote/firebase_auth_source.dart
+++ lib/data/db/remote/firebase_auth_source.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:tinder_new/data/db/remote/response.dart';
 
 class FirebaseAuthSource {
@@ -10,19 +11,33 @@ class FirebaseAuthSource {
           email: email, password: password);
       return Response.success(userCredential);
     } catch (e) {
-      return Response.error(
-          ((e as FirebaseException).message ?? e.toString()));
+      return Response.error(((e as FirebaseException).message ?? e.toString()));
     }
   }
 
-  Future<Response<UserCredential>> register(String email, String password) async {
+  Future<Response<UserCredential>> register(
+      String email, String password) async {
     try {
       UserCredential userCredential = await instance
           .createUserWithEmailAndPassword(email: email, password: password);
       return Response.success(userCredential);
     } catch (e) {
-      return Response.error(
-          ((e as FirebaseException).message ?? e.toString()));
+      return Response.error(((e as FirebaseException).message ?? e.toString()));
+    }
+  }
+
+  Future<Response<UserCredential>> signInWithGoogle(
+      GoogleSignInAccount account) async {
+    try {
+      final googleSignInAuthentication = await account.authentication;
+      final oAuthCredential = GoogleAuthProvider.credential(
+          idToken: googleSignInAuthentication.idToken,
+          accessToken: googleSignInAuthentication.accessToken);
+      UserCredential userCredential =
+          await instance.signInWithCredential(oAuthCredential);
+      return Response.success(userCredential);
+    } catch (e) {
+      return Response.error(((e as FirebaseException).message ?? e.toString()));
     }
   }
 }
